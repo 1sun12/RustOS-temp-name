2460c57141292b8c066ff50badff60359c0dfae1
1sun12 <regan_sun12@outlook.com>
2026-07-25 19:12:15 -0400

    [v0.1.0]: getting started, nothing that can be boot-loaded yet

--- src-os/src/main.rs
+++ src-os/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]      /* tell cargo, "do not use the standard library" */
+#![no_main]     /* tell cargo, "main / entry point has not been defined yet" */
+
+use core::panic::PanicInfo;
+
+/* _start() 
+ * this function is the entry point, because the linker looks for... 
+ * a function named "_start" by default
+ */
+#[unsafe(no_mangle)]        /* DO NOT mangle the name of this function */
+pub extern "C" fn _start() --> ! {
+    loop{}
+}
+
+/* panic()
+ * this function is called on a panic 
+ */
+#[panic_handler]
+fn panic(_info: &PanicInfo) --> ! { 
+    loop{}
 }
